c5f74da89136625524dc8601a639f4cd27635d33
biezhihua <biezhihua@gmail.com>
2020-02-02 22:44:38 +0800

    feat-flutter-ios: run xcode

--- libdsm_flutter/libdsm/ios/Classes/SwiftDsmPlugin.swift
+++ libdsm_flutter/libdsm/ios/Classes/SwiftDsmPlugin.swift
@@ -2,6 +2,18 @@ import Flutter
 import UIKit
 
 public class SwiftDsmPlugin: NSObject, FlutterPlugin {
+    
+    static  let ID = "id"
+    static  let TIME_OUT = "time_out"
+    static  let NAME = "name"
+    static  let ADDRESS = "address"
+    static  let HOST = "host"
+    static  let LOGIN_NAME = "login_name"
+    static  let PASSWORD = "password"
+    static  let TID = "tid"
+    static  let PATTERN = "pattern"
+    static  let PATH = "path"
+    
     public static func register(with registrar: FlutterPluginRegistrar) {
         let channel = FlutterMethodChannel(name: "open.flutter/libdsm", binaryMessenger: registrar.messenger())
         let instance = SwiftDsmPlugin()
@@ -10,13 +22,14 @@ public class SwiftDsmPlugin: NSObject, FlutterPlugin {
     
     public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
         
-        NSLog("call = \(call) result = \(result) biehzihua ")
+        NSLog("call.method = \(call.method) call.arguments = \(call.arguments) biezhihua")
         
-        if (call.method == "DSM_create"){
-            result("iOS biezhihua")
-        }else {
-            result("iOS " + UIDevice.current.systemVersion)
+        switch call.method {
+        case "DSM_init":
+            result(nil)
+        default:
+            result(FlutterMethodNotImplemented)
         }
-        
+
     }
 }
