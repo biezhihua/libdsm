3484e2e08c2e0e09aca9150ad3969d038d3fbf1b
biezhihua <biezhihua@gmail.com>
2020-02-02 19:38:00 +0800

    fix: runner

--- libdsm_flutter/libdsm/ios/Classes/SwiftDsmPlugin.swift
+++ libdsm_flutter/libdsm/ios/Classes/SwiftDsmPlugin.swift
@@ -2,18 +2,21 @@ import Flutter
 import UIKit
 
 public class SwiftDsmPlugin: NSObject, FlutterPlugin {
-  public static func register(with registrar: FlutterPluginRegistrar) {
-    let channel = FlutterMethodChannel(name: "open.flutter/libdsm", binaryMessenger: registrar.messenger())
-    let instance = SwiftDsmPlugin()
-    registrar.addMethodCallDelegate(instance, channel: channel)
-  }
-
-  public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-      if (call.method == "DSM_create"){
-        result("iOS biezhihua")
-      }else {
-        result("iOS " + UIDevice.current.systemVersion)
-      }
+    public static func register(with registrar: FlutterPluginRegistrar) {
+        let channel = FlutterMethodChannel(name: "open.flutter/libdsm", binaryMessenger: registrar.messenger())
+        let instance = SwiftDsmPlugin()
+        registrar.addMethodCallDelegate(instance, channel: channel)
+    }
     
-  }
+    public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+        
+        NSLog("call = \(call) result = \(result)  ")
+        
+        if (call.method == "DSM_create"){
+            result("iOS biezhihua")
+        }else {
+            result("iOS " + UIDevice.current.systemVersion)
+        }
+        
+    }
 }
