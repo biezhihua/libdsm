7fd74935884e0830f1f46f65403e74a1f1b0c13d
biezhihua <biezhihua@gmail.com>
2021-04-04 20:59:23 +0800

    fix: flutter ios

--- libdsm_flutter/libdsm/example/ios/Runner/AppDelegate.swift
+++ libdsm_flutter/libdsm/example/ios/Runner/AppDelegate.swift
@@ -3,29 +3,11 @@ import Flutter
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
-    override func application(
-        _ application: UIApplication,
-        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-    ) -> Bool {
-        GeneratedPluginRegistrant.register(with: self)
-        
-        let urlString:String = "https://8.8.8.8"
-        
-        let url = URL(string: urlString)
-        
-        let request = URLRequest(url: url!)
-        
-        let config = URLSessionConfiguration.default
-        
-        let session = URLSession(configuration: config)
-        
-        let task = session.dataTask(with: request) { (data,response,error) in
-            
-            
-        }
-        
-        task.resume()
-        
-        return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-    }
+  override func application(
+    _ application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+  ) -> Bool {
+    GeneratedPluginRegistrant.register(with: self)
+    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
 }
